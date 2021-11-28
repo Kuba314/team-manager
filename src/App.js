a38bd7536a2de9cbcafbd0ec757f3df2df58cc4b
Jakub Kozubek <jakub.kozubek@gmail.com>
2021-11-28 16:51:25 +0100

    Fixed warnings, fixed scrolling background bug, improved the ppereance of the discussion page

--- src/App.js
+++ src/App.js
@@ -4,38 +4,30 @@ import DiscussionPage from "./Pages/DiscussionPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Components/Layout";
 import { createTheme, ThemeProvider } from "@mui/material";
-import { makeStyles } from "@mui/styles";
+
 import RegisterPage from "./Pages/RegisterPage";
 import React from 'react';
-import {useState, useMemo} from 'react';
+import {useState} from 'react';
 import LoginPage from "./Pages/LoginPage";
+import { CssBaseline } from "@mui/material";
 
 
 const theme = createTheme({
   palette: {
     mode: "dark",
-    background: {
-      color: "#222222",
-    },
-  },
-  background: "#222222",
-});
+    primary: {
+      main: "#FFF"
+    }
+}});
 
-const useStyles = makeStyles({
-  cont: {
-    background: "#555555",
-    margin: 0,
-    height: "100%",
-  },
-});
 
 function App() {
   const [logged, setLogged] = useState(false);
-  const classes = useStyles();
+
 
   return (
-      <div className={classes.cont}>
       <ThemeProvider theme={theme}>
+      <CssBaseline />
         <BrowserRouter>
           <Layout>
             <Routes>   
@@ -47,7 +39,7 @@ function App() {
           </Layout>
         </BrowserRouter>
       </ThemeProvider>
-      </div>
+
 
   );
 }
--- src/Components/AddDialog.js
+++ src/Components/AddDialog.js
@@ -13,7 +13,7 @@ import { makeStyles } from "@mui/styles";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
+
 import DialogTitle from "@mui/material/DialogTitle";
 
 const useStyles = makeStyles({
