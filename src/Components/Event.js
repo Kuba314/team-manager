a547c867a9c3a9f74b527fe12540073b443881d1
Jakub Kozubek <jakub.kozubek@gmail.com>
2021-12-02 00:23:59 +0100

    Added buttons for coming or not coming, now just switching colors

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import PlaceIcon from "@mui/icons-material/Place";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import DoneIcon from "@mui/icons-material/Done";
 import {
   Card,
   CardHeader,
@@ -19,21 +20,42 @@ import {
   Grid,
 } from "@mui/material";
 import AttendanceAvatar from "./AttendanceAvatar";
+import CloseIcon from "@mui/icons-material/Close";
+import { makeStyles } from "@mui/styles";
+const useStyles = makeStyles({
+  notComeBtn: {
+    //backgroundColor: "",
+    backgroundColor: "#856565",
+    "&:hover": {
+      backgroundColor: "#cc6262",
+    },
+  },
+  comeBtn: {
+    backgroundColor: "#6a8565",
+    "&:hover": {
+      backgroundColor: "#67cc62",
+    },
+  },
+});
 
 function Event({ event }) {
+  let selred = "#eb5e5e";
+  let red = "#856565";
+  let selgreen = "#67cc62";
+  let green = "#6a8565";
+  let gray = "#8e918f";
+  const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
+  const [colorCome, setColor] = useState("#6a8565");
+  const [colorNotCome, setNotColor] = useState("#856565");
   return (
     <div>
       <Card align="center">
-        <CardHeader
-          title={event.title}
-          avatar={<Avatar>{event.author[0]}</Avatar>}
-          subheader={event.category}
-        />
+        <CardHeader title={event.title} subheader={event.category} />
 
         <CardContent>
           <List>
@@ -54,6 +76,38 @@ function Event({ event }) {
         <CardActions>
           <Button size="large" expand={expanded} onClick={handleExpandClick}>
             Kdo přijde
+          </Button>
+          <Button
+            size="large"
+            variant="contained"
+            style={{ backgroundColor: colorCome }}
+            onClick={() => {
+              if (colorCome === selgreen) {
+                setNotColor(red);
+                setColor(green);
+                return;
+              }
+              setColor(selgreen);
+              setNotColor(gray);
+            }}
+          >
+            <DoneIcon></DoneIcon>Přijdu
+          </Button>
+          <Button
+            size="large"
+            variant="contained"
+            style={{ backgroundColor: colorNotCome }}
+            onClick={() => {
+              if (colorNotCome === selred) {
+                setNotColor(red);
+                setColor(green);
+                return;
+              }
+              setNotColor(selred);
+              setColor(gray);
+            }}
+          >
+            <CloseIcon></CloseIcon>Nepřijdu
           </Button>
         </CardActions>
         <Collapse in={expanded} timeout="auto" unmountOnExit>
